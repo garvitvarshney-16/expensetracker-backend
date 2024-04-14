7629e7268faef71c0cbd3ed094b96cefeef63957
garvitvarshney-16 <garvitvarshney81@gmail.com>
2024-04-14 17:58:23 +0530

    ok

--- src/db/index.js
+++ src/db/index.js
@@ -1,7 +1,7 @@
 import Sequelize from "sequelize";
 
 export const sequelize = new Sequelize("expense", "root", "", {
-  host: "localhost",
+  host: "127.0.0.1",
   logging: false,
   dialect: "mysql",
 });
